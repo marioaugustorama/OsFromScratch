84ed3107666095df6243434a78a0e91b62348bc4
Mário Nunes <mariogar1979@gmail.com>
2015-04-25 01:03:58 -0300

    Adicionado dump dos primeiros 512 bytes em Hexa ao termino da gravação do bootloader

--- loader/bootwriter.c
+++ loader/bootwriter.c
@@ -6,8 +6,8 @@
 int main(int argc, char *argv[])
 {
 	char bufb[512];
-	int fdisk;
-	int bdisk;
+	int fdisk, bdisk, i, j;
+	unsigned char ch;
 	
 	if(argc<3) {
 		printf("\nBoot Writer\n");
@@ -30,6 +30,26 @@ int main(int argc, char *argv[])
 	write(fdisk,bufb,512);
 	printf("\n * OK .. Bootsector gravado com sucesso\n\n");
 	close(fdisk);
+
+	printf("boot sector in HEX:\n");
+	j=0;
+	for(i=0; i<512; i++)
+	{
+		ch=(unsigned char) bufb[i];
+		if (j==8) printf(" -");
+		if (j==16) 
+		{
+			printf("\n");
+			j=0;
+		}
+		j++;
+		if (ch<0x10) printf(" 0%x",ch);
+		else printf(" %2x",ch);
+	}
+
+	printf("\n\n");
+
+
 
 	}
 	
